38521b021ddfd82b0e9ecd2584e843ddc0d82aaf
Matthew Johnston <mattdjohnston92@gmail.com>
2020-10-25 11:05:41 -0400

    Add updated study notes and js exercise files.

---
+++ lesson_1_js_basics/exercise.js
@@ -0,0 +1,21 @@
+// function multiply(num1, num2) {
+//   return num1 * num2;
+// }
+
+// function getNumber(prompt) {
+//   let rlSync = require('readline-sync');
+//   let number = rlSync.question(prompt);
+//   return number;
+// }
+
+// let num1 = getNumber('Enter the first number: ');
+// let num2 = getNumber('Enter the second number: ');
+
+// console.log(`${num1} * ${num2} = ${multiply(num1, num2)}`);
+
+
+function scream(words) {
+  return words + '!!!!';
+}
+
+scream('Yipeee');
--- lesson_1_js_basics/greeter.js
+++ lesson_1_js_basics/greeter.js
@@ -1,5 +1,9 @@
-let rlSync = require('readline-sync');
+function getName(prompt) {
+  let readlineSync = require('readline-sync');
+  let name = readlineSync.question(prompt);
+  return name;
+}
 
-let firstName = rlSync.question('What is your first name? ');
-let lastName = rlSync.question('What is your last name? ');
+let firstName = getName('What is your first name? ');
+let lastName = getName('What is your last name? ');
 console.log(`Hello, ${firstName} ${lastName}!`);
