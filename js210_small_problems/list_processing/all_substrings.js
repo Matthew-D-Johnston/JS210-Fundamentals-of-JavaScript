e6aab347a236fe16d722568a9229ed20b4191273
Matthew Johnston <mattdjohnston92@gmail.com>
2021-03-17 11:36:55 -0400

    Add js exercise files.

---
+++ js210_small_problems/list_processing/all_substrings.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// function substrings(string) {
+//   const Results = [];
+
+//   for (let index = 0; index < string.length; index += 1) {
+//     let substring = string.slice(index);
+//     let substringsArray = leadingSubstrings(substring);
+//     Results.push(substringsArray);
+//   }
+
+//   return Results.flat();
+// }
+
+
+function substrings(string) {
+  let chars = string.split('');
+
+  let substringsArray = chars.map((char, index) => {
+    return leadingSubstrings(chars.slice(index).join(''));
+  });
+
+  return substringsArray.flat();
+}
+
+function leadingSubstrings(string) {
+  const Subsets = [];
+
+  for (let index = 0; index < string.length; index += 1) {
+    Subsets.push(string.slice(0, index + 1));
+  }
+
+  return Subsets;
+}
+
+console.log(substrings('abcde'));
